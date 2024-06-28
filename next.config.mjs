111302ac88c9a3a18987c9478e425254cb58a650
Mohammad Erfan Akbari <m.erfan.ak@gmail.com>
2024-06-28 10:47:30 +0330

    Try to handle some bug in vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      "/order": ["./data/*"],
-    },
-    outputFileTracingRoot: path.join(__dirname, "./data/db.json"),
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
